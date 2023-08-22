946da11924c9482421442ae92544d6cb1cb91f8a
Nayeob <108695511+Raccoonmen@users.noreply.github.com>
2023-08-22 15:50:21 +0900

    Initial commit

---
+++ src/main/java/com/riot_gaming/RiotGamingApplication.java
@@ -0,0 +1,13 @@
+package com.example.riot_gaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiotGamingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RiotGamingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/riot_gaming/RiotGamingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.riot_gaming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiotGamingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
